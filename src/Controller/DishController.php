2ff9eb0be658590b2a601d3039b76458936e2bfa
lutfiqaraman <lutfiqaraman@gmail.com>
2022-03-02 22:16:13 +0300

    create dish controller

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Dishes;
+use App\Repository\DishesRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/dish', name: 'dish.')]
 class DishController extends AbstractController
 {
     private EntityManagerInterface $em;
@@ -19,14 +20,17 @@ class DishController extends AbstractController
         $this->em = $em;
     }
 
-    #[Route('/dish', name: 'dish')]
-    public function index(): Response
+    #[Route('/', name: 'edit')]
+    public function index(DishesRepository $dishesRepository): Response
     {
+        $dishes = $dishesRepository->findAll();
+
         return $this->render('dish/index.html.twig', [
-            'controller_name' => 'DishController',
+            'dishes' => $dishes,
         ]);
     }
 
+    #[Route('/create', name: 'create')]
     public function create(Request $request) {
         $dish = new Dishes();
         $dish->setName('Pizza');
@@ -34,5 +38,6 @@ class DishController extends AbstractController
         $this->em->persist($dish);
         $this->em->flush();
 
+        return new Response("Dish has been created");
     }
 }
