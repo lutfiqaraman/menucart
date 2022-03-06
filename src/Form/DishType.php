ba69d1dbe3defc35793e4a7a1a3afd8bba18e295
lutfiqaraman <lutfiqaraman@gmail.com>
2022-03-06 22:57:09 +0300

    add image property to the form that create a dish

--- src/Form/DishType.php
+++ src/Form/DishType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Dishes;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,6 +17,7 @@ class DishType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
+            ->add('image', FileType::class)
             ->add('save', SubmitType::class)
         ;
     }
