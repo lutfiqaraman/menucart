4a9d010bdb0118c780bbb424824bf61dbbe5f950
lutfiqaraman <lutfiqaraman@gmail.com>
2022-03-31 22:02:01 +0300

    building registration form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'register')]
-    public function register(): Response
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $hashingUserPassword;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $hashingUserPassword)
     {
+        $this->em = $em;
+        $this->hashingUserPassword = $hashingUserPassword;
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(Request $request): Response
+    {
+        $user = new User();
+
+        $registerForm = $this->createForm(RegisterType::class, $user);
+        $registerForm->handleRequest($request);
+
+        if ($registerForm->isSubmitted() && $registerForm->isValid())
+        {
+            $user->setPassword(
+                $this->hashingUserPassword->hashPassword(
+                    $user, $user->getPassword()
+                )
+            );
+
+            $this->em->persist($user);
+            $this->em->flush();
+        }
+
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'registerForm' => $registerForm->createView()
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +16,12 @@ class RegisterType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('roles')
-            ->add('password')
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password')
+            ))
+            ->add('Register', SubmitType::class)
         ;
     }
 
