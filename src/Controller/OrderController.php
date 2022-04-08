a15b3d7673354d42b07748c5c91f94b7495b7ed4
lutfiqaraman <lutfiqaraman@gmail.com>
2022-04-08 22:57:03 +0300

    build order function

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,12 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Dishes;
 use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/order', name: 'order')]
     public function index(): Response
     {
@@ -19,8 +28,20 @@ class OrderController extends AbstractController
     }
 
     #[Route('/order/{id}', name: 'order')]
-    public function order(Dishes $dish)
+    public function order(Dishes $dish) : RedirectResponse
     {
         $order = new Order();
+
+        $order->setTablename('Table 1');
+        $order->setName($dish->getName());
+        $order->setOrdernumber($dish->getId());
+        $order->setPrice($dish->getPrice());
+        $order->setStatus('Open');
+
+        $this->em->persist($order);
+        $this->em->flush();
+
+        return
+            $this->redirect($this->generateUrl('menu'));
     }
 }
