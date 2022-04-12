87630a124f360887c62c1e111d01327595923a1b
lutfiqaraman <lutfiqaraman@gmail.com>
2022-04-12 21:33:07 +0300

    build mail function

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MailerController extends AbstractController
 {
     #[Route('/mail', name: 'mail')]
-    public function index(): Response
+    public function sendEmail(MailerInterface $mailer): Response
     {
         return $this->render('mailer/index.html.twig', [
             'controller_name' => 'MailerController',
